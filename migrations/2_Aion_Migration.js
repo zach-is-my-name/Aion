c47d22a22edc80917424f3de1f7da6501be46be9
zach-is-my-name <zach.greco@gmail.com>
2020-09-06 01:00:31 -0400

    changed migration

--- migrations/2_Aion_Migration.js
+++ migrations/2_Aion_Migration.js
@@ -1,5 +1,9 @@
 const Aion = artifacts.require("Aion");
+const SafeMath = artifacts.require("SafeMath") 
+const AionClient = artifacts.require("AionClient")
 module.exports = function(deployer) {
   deployer.deploy(Aion);
+  deployer.deploy(SafeMath);
+  deployer.deploy(AionClient);
   };
 
