f3dd8b9a1ed52fe39226b36ca64b61ec02871279
zach-is-my-name <zach.greco@gmail.com>
2021-03-30 14:37:17 -0400

    changed mind, going with Aion.sol Solidity .05; bumped truffle-config compiler version; replaced Aion.sol with v.05; added SafeMath.sol for import

--- contracts/Aion.sol
+++ contracts/Aion.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.0; 
 
-import "./GoalEscrowTestVersion.sol"; /*debug*/
 import "./SafeMath.sol";
 
 /* ----------------------------------------------------------------------------
