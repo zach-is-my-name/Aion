8956a5c7b888c4f435cdf1c5c619924ff222446e
zach-is-my-name <zach.greco@gmail.com>
2021-04-03 10:49:03 -0400

    removed safemath from migrations; artifact from bfg scrub (?)

--- migrations/2_Aion_Migration.js
+++ migrations/2_Aion_Migration.js
@@ -1,6 +1,6 @@
 const Aion = artifacts.require("Aion");
 const AionClient = artifacts.require("AionClient")
-module.exports = function(deployer, ropsten, accounts ) {
+module.exports = function(deployer) {
   deployer.deploy(Aion)
   };
 
