ce567b6936cd783344448eabf1b9c703454dc455
zach-is-my-name <zach.greco@gmail.com>
2021-04-03 10:30:29 -0400

    removed safemath contract deploy

--- migrations/2_Aion_Migration.js
+++ migrations/2_Aion_Migration.js
@@ -1,9 +1,6 @@
 const Aion = artifacts.require("Aion");
-const SafeMath = artifacts.require("SafeMath") 
 const AionClient = artifacts.require("AionClient")
 module.exports = function(deployer, ropsten, accounts ) {
-  deployer.deploy(SafeMath, {from: accounts[9]});
-  deployer.deploy(Aion, {from: accounts[9]}).then(function() {
-    return deployer.deploy(AionClient, Aion.address, {from: accounts[9]})});
+  deployer.deploy(Aion)
   };
 
