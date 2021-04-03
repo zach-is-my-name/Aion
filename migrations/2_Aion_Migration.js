832e11f5ab0aa954e73051965f9f8e1a27edda29
zach-is-my-name <zach.greco@gmail.com>
2021-04-03 16:05:00 -0400

    abandon truffle-teams (ci unreliable / down); deploy (ropsten) from truffle directly

--- migrations/2_Aion_Migration.js
+++ migrations/2_Aion_Migration.js
@@ -1,6 +1,5 @@
 const Aion = artifacts.require("Aion");
 
-
 module.exports = function(deployer) {
   deployer.deploy(Aion)
   };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config()
+const privateKey = process.env.PK
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -67,14 +69,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+     ropsten: {
+     provider: () => new HDWalletProvider({privateKeys: [privateKey], providerOrUrl: `https://ropsten.infura.io/v3/aa35f14f27e9485eab995085f4b77150`}),
+     network_id: 3,       // Ropsten's id
+     gas: 7000000,        // Ropsten has a lower block limit than mainnet
+     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+     timeoutBlocks: 10,  // # of blocks before a deployment times out  (minimum/default: 50)
     // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+     },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
